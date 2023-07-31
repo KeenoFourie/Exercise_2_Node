ec80a69cce2f2b9b5424e9190fe736e63ec827b9
KeenoFourie <keenofourie614@gmail.com>
2023-07-31 15:44:44 +0200

    update

---
+++ index.js
@@ -0,0 +1,10 @@
+const person = require('./person')
+
+const person1 = new person('Keeno', 'Fourie', 'keenofourie614@gmail.com')
+console.log(person1);
+
+const person2 = new person('Codi', 'Kader', 'codikader45@gmail.com')
+console.log(person2);
+
+const person3 = new person('Taflynne Summer', 'Petersen', 'tafsummerpetersen@gmail.com')
+console.log(person3);
